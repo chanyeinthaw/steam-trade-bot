011a7f019c63d751448062a56329a7e0b73c7180
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-04-20 14:37:32 +0630

    Added: gamesparks login api action Added: gamesparks register api action

---
+++ modules/gamesparks-endpoint/api-actions/register.js
@@ -0,0 +1,21 @@
+const Requests = require('../../gamesparks-endpoint').Requests;
+
+module.exports = (req, res, modules) => {
+	let query = req.query;
+
+	if (query.hasOwnProperty('username') && query.hasOwnProperty('password') && query.hasOwnProperty('displayname')) {
+		modules.gamesparks.register(query.displayname, query.username, query.password, (err, body) => {
+			if (!err) {
+				res.send(body);
+			} else {
+				res.send(err.message);
+			}
+		});
+
+		return;
+	}
+
+	res.send({
+		error: 'invalid inputs'
+	});
+};
