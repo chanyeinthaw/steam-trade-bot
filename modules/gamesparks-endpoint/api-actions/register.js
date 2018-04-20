5a35ac473eb75a4020cfaaceceb4574f4fd3554e
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-04-20 15:22:15 +0630

    Handled: method name wrong error

--- modules/gamesparks-endpoint/api-actions/register.js
+++ modules/gamesparks-endpoint/api-actions/register.js
@@ -4,7 +4,7 @@ module.exports = (req, res, modules) => {
 	let query = req.query;
 
 	if (query.hasOwnProperty('username') && query.hasOwnProperty('password') && query.hasOwnProperty('displayname')) {
-		modules.gamesparks.register(query.displayname, query.username, query.password, (err, body) => {
+		modules.gamesparks.registerUser(query.displayname, query.username, query.password, (err, body) => {
 			if (!err) {
 				res.send(body);
 			} else {
