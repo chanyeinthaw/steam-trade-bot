ca8d7d6450ce87e9a6d42b528a85ad00c6e61319
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-04-30 15:56:58 +0630

    Added: send-item action (untested)

--- modules/api-endpoint/actions/send-items.js
+++ modules/api-endpoint/actions/send-items.js
@@ -13,11 +13,38 @@ module.exports = (req, res, modules) => {
 		return res.send(errors);
 	}
 
-	if (apikey !== 'foolish') {
+	if (query.apikey !== 'foolish') {
 		return res.send({
 			error: 'Invalid API key.'
 		});
 	}
 
+	if (modules.registry.getIdleBotCount() <= 0) {
+		return res.send({
+			error: 'Bots are busy or offline.'
+		});
+	}
 
+	let idleBot = modules.registry.getIdleBot();
+
+	let message = Buffer.from(idleBot.getBotName() + Date.now().toString())
+		.toString('base64')
+		.replace(/=/g, '');
+
+	idleBot.sendTradeOffer(toSteamid(query.partner),
+		query.token,
+		[],
+		JSON.parse(query.items),
+		message,
+		(err, body) => {
+			if (err) {
+				res.send(err.message);
+			} else {
+
+
+				res.send(JSON.stringify(body));
+			}
+		});
+
+	idleBot.releaseBot();
 };
