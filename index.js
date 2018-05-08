af062753f57741d4b300216654fed16e0d86aba2
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-05-08 15:52:31 +0630

    Used Data and Mysql

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Fs = require('fs');
+const Mysql = require('promise-mysql');
 const TradeBot = require('./modules/trade-bot');
 const APIEndpoint = require('./modules/api-endpoint');
 const GamesparksEndpoint = require('./modules/gamesparks-endpoint');
+const Data = require('./data');
 
 const ENV = JSON.parse(Fs.readFileSync("env.json"));
 
@@ -11,16 +13,20 @@ process.on('unhandledRejection', error => {
 
 class SteamTradeBot {
 	constructor() {
-		this.gamesparks = new GamesparksEndpoint.Gamesparks(ENV.gamesparks);
-		this.botRegistry = new TradeBot.Registry();
-		this.apiEndpoint = new APIEndpoint(3000, {
-			registry: this.botRegistry,
-			gamesparks: this.gamesparks
+		Mysql.createConnection(ENV.mysql).then((conn) => {
+			this.gamesparks = new GamesparksEndpoint.Gamesparks(ENV.gamesparks);
+			this.botRegistry = new TradeBot.Registry();
+			this.apiEndpoint = new APIEndpoint(3000, {
+				registry: this.botRegistry,
+				gamesparks: this.gamesparks,
+				mysql: conn,
+				Data: Data,
+			});
+
+			this.registerBotList();
+
+			this.apiEndpoint.listen();
 		});
-
-		this.registerBotList();
-
-		this.apiEndpoint.listen();
 	}
 
 	registerBotList() {
