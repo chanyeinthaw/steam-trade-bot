604144aaf5fff9bba09e950d0ba54a5e51f48edd
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-05-08 15:53:00 +0630

    request-item action (need testing)

--- modules/api-endpoint/actions/request-items.js
+++ modules/api-endpoint/actions/request-items.js
@@ -2,6 +2,7 @@ const validate = require('../validate');
 const toSteamid = require('../../to-steamid');
 
 module.exports = async (req, res, modules) => {
+	//region validation
 	let query = req.query;
 
 	let requires = [
@@ -10,15 +11,10 @@ module.exports = async (req, res, modules) => {
 
 	let errors = validate(requires, query);
 
-	if (errors) {
-		return res.send(errors);
-	}
+	if (errors) return res.send(errors);
+	//endregion
 
-	if (modules.registry.getIdleBotCount() <= 0) {
-		return res.send({
-			error: 'Bots are busy or offline.'
-		});
-	}
+	if (modules.registry.getIdleBotCount() <= 0) return res.send({error: 'Bots are busy or offline.'});
 
 	let idleBot = modules.registry.getIdleBot();
 
@@ -26,8 +22,33 @@ module.exports = async (req, res, modules) => {
 		.toString('base64')
 		.replace(/=/g, '');
 
+	let pdao = new modules.Data.PendingTradesDao(modules.mysql);
+	let adao = new modules.Data.AllowedItemsDao(modules.mysql);
+
 	try {
-		let body = await idleBot.sendTradeOffer(toSteamid(query.partner), query.token, JSON.parse(query.items), [], message);
+		let items = JSON.parse(query.items);
+		let allowItems = [];
+		let allowedAssets = await adao.checkItems(items); // get allowed assetids of items array from database;
+
+		// region check if item is allowed
+		for(let i = 0; i < items.length; i++) {
+			let assetId = items[i].assetid;
+			let shouldAdd = false;
+
+			for(let ii = 0; ii < allowedAssets.length; ii++) {
+				if (assetId == allowedAssets[ii].assetid) {
+					shouldRemove = true; break;
+				}
+			}
+
+			if (shouldAdd) allowItems.push(items[i]);
+		}
+		//endregion
+
+		let body = await idleBot.sendTradeOffer(toSteamid(query.partner), query.token, allowItems, [], message);
+
+		if (body.hasOwnProperty('tradeofferid'))
+			pdao.addTradeOffer(body.tradeofferid, idleBot.getBotName(), query.items, 'in');
 
 		res.send(JSON.stringify(body));
 	} catch(e) {
