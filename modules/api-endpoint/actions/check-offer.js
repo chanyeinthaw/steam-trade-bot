5409c9b398be53448506c77606dae434f1c348f6
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-05-10 15:11:32 +0630

    added offer object to message response object

--- modules/api-endpoint/actions/check-offer.js
+++ modules/api-endpoint/actions/check-offer.js
@@ -2,9 +2,10 @@ const validate = require('../validate');
 const toSteamid = require('../../to-steamid');
 
 class Msg {
-	constructor(message, state) {
+	constructor(message, state, offer) {
 		this.message = message;
 		this.state = state;
+		this.offer = offer;
 	}
 }
 
@@ -29,7 +30,7 @@ module.exports = async (req, res, modules) => {
 	try {
 		let check = await pendingTrade.checkTradeOffer(query.offerid);
 
-		if (check.length <= 0) return res.send(new Msg('Trade offer not found.', -1));
+		if (check.length <= 0) return res.send(new Msg('Trade offer not found.', -1, null));
 
 		let row = check[0];
 
@@ -40,7 +41,6 @@ module.exports = async (req, res, modules) => {
 		if (offer.trade_offer_state === 3) {
 			let items = JSON.parse(row.items);
 			let isIncomingOffer = row.in_out === 'in';
-
 			let totalCoins = await allowedItem.getTotalCoins(items);
 
 			// TODO add to own inventory
@@ -54,19 +54,19 @@ module.exports = async (req, res, modules) => {
 
 			pendingTrade.deleteTradeOffer(query.offerid);
 
-			return res.send(new Msg(`Trade offer ${query.offerid} accepted. Account credited`, offer.trade_offer_state));
+			return res.send(new Msg(`Trade offer ${query.offerid} accepted. Account credited`, offer.trade_offer_state, offer));
 		} else if (offer.trade_offer_state === 6 || offer.trade_offer_state === 5 || offer.trade_offer_state === 7) {
 			pendingTrade.deleteTradeOffer(query.offerid);
 
-			return res.send(new Msg(`Trade offer ${query.offerid} cancled or expired.`, offer.trade_offer_state));
+			return res.send(new Msg(`Trade offer ${query.offerid} cancled or expired.`, offer.trade_offer_state, offer));
 		} else if (offer.trade_offer_state === 2 || offer.trade_offer_state === 9) {
-			return res.send(new Msg(`Trade offer ${query.offerid} active.`, offer.trade_offer_state));
+			return res.send(new Msg(`Trade offer ${query.offerid} active.`, offer.trade_offer_state, offer));
 		} else {
 			pendingTrade.deleteTradeOffer(query.offerid);
 
-			return res.send(new Msg(`Trade offer ${query.offerid} no longer valid.`, offer.trade_offer_state));
+			return res.send(new Msg(`Trade offer ${query.offerid} no longer valid.`, offer.trade_offer_state, offer));
 		}
 	} catch (e) { console.log(e) ;}
 
-	res.send(new Msg(`An error occurred.`, -3));
+	res.send(new Msg(`An error occurred.`, -3, null));
 };
