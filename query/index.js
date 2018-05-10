4d311a112b9751a447dd7cd07cd5f3b41c72ce0a
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-05-10 13:47:40 +0630

    old query deleted

--- query/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	PendingTrade: require('./pending-trade.js'),
-	AllowedItem: require('./allowed-item.js'),
-	InventoryItem: require('./inventory-items.js')
-};
--- query/inventory-items.js
+++
@@ -1,9 +0,0 @@
-const QUERY = {
-	insert: 'insert into inventory_items set ?'
-};
-
-class InventoryItemDao {
-	constructor(conn) {
-		this.conn = conn;
-	}
-}
