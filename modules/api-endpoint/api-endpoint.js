877d8165957cdaa909b77f0bd36e194e407134d1
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-05-11 10:47:21 +0630

    added jwtAuth middleware to endpoint removed unused middlewares from routes

--- modules/api-endpoint/api-endpoint.js
+++ modules/api-endpoint/api-endpoint.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const routes = require('./routes.js');
+
+class Err {
+	constructor(msg) {
+		this.error = msg;
+	}
+}
+
 class APIEndpoint {
 	constructor (port) {
 		this.port = port;
 		this.app = express();
+
+		this.registerJwtAuth();
+	}
+
+	registerJwtAuth() {
+		this.app.use((req, res, next) => {
+			if (!req.query.hasOwnProperty('session_id')) return res.send(new Err('session_id required'));
+
+			try {
+				req.loggedUserId = jwt.verify(req.query.session_id, global.jwtSecret).sub;
+				return next();
+			} catch (e) {
+				return res.send(new Err(e.message));
+			}
+		});
 	}
 
 	run() {
 		routes(this.app);
+
 		let server = this.app.listen(this.port, () => {
 			console.log(`API Endpoint started at port: ${server.address().port}`);
 		});
