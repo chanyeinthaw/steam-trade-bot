708e1389c1f12e8563e0934b1bb0107c93c6d5e9
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2018-05-11 10:46:35 +0630

    added user query with method to get gamesparks password decrypted

--- modules/database/index.js
+++ modules/database/index.js
@@ -2,6 +2,8 @@ module.exports = {
 	pendingTrade: require('./query/pending-trade.js'),
 	allowedItem: require('./query/allowed-item.js'),
 	inventoryItem: require('./query/inventory-item.js'),
+	user: require('./query/user.js'),
+
 	async connection() {
 		return await require('./connector.js')(global.mysqlConfig);
 	}
